71bb97151f2d8cb88e516d132bcf8349fbb7f216
aliv59git <vilavaln@gmail.com>
2015-05-24 23:30:12 +0300

    new

---
+++ Home1DataTypesAndVariables/1.JavaScriptLiterals/js/main.js
@@ -0,0 +1,41 @@
+﻿
+var intNumb = 5;
+jsConsole.writeLine(intNumb);
+
+
+
+
+
+
+
+
+
+
+
+
+function timerFunc() {
+    var now = new Date();
+    var hours = now.getHours();
+    if (hours < 10) {
+        hours = "0" + hours;
+    }
+    var minutes = now.getMinutes();
+    if (minutes < 10) {
+        minutes = "0" + minutes;
+    }
+    var seconds = now.getSeconds();
+    if (seconds < 10) {
+        seconds = "0" + seconds;
+    }
+    document.getElementById("timer").innerHTML = hours + " : " + minutes + " : " + seconds;
+
+}
+
+var timer;
+
+function start() {
+    timer=setInterval(timerFunc, 1000);
+}
+function stop() {
+    clearInterval(timer);
+}
