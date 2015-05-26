cba54e66a83688ad402f6175854cff06b90cfdcf
aliv59git <vilavaln@gmail.com>
2015-05-26 16:32:43 +0300

    new version now

---
+++ Home2OperatorsAndExpressions/1.OddOrEven/scripts/1.OddOrEven.js
@@ -0,0 +1,12 @@
+﻿/*•	Write an expression that checks if given integer is odd or even.*/
+
+function oddOrEven() {
+    var strN = document.getElementById("input-text").value;
+    var numb = strN|0;
+    if (numb % 2) {
+        jsConsole.writeLine("true");
+    } else {
+        jsConsole.writeLine("false");
+    }
+
+}
---
+++ Home2OperatorsAndExpressions/1.OddOrEven/scripts/2.DivisibleBy7And5.js
@@ -0,0 +1,12 @@
+﻿/*•	Write a boolean expression that checks for given integer if it can be divided 
+(without remainder) by 7 and 5 in the same time.*/
+
+function divisionBy7And5() {
+    var str = document.getElementById("input-text").value;
+    var numb = str|0;
+    if ((numb % 5===0) && (numb % 7===0)) {
+        jsConsole.writeLine("true");
+    } else {
+        jsConsole.writeLine("false");
+    }
+}
