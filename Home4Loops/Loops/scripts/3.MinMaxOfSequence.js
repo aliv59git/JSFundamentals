90e34759cc46f2cf608d87897d8824b54ff92cd8
aliv59git <vilavaln@gmail.com>
2015-05-30 00:02:19 +0300

    var new f

--- Home4Loops/Loops/scripts/3.MinMaxOfSequence.js
+++ Home4Loops/Loops/scripts/3.MinMaxOfSequence.js
@@ -1,11 +1,22 @@
 ﻿/*•	Write a script that finds the max and min number from a sequence of numbers.*/
 
 function minMaxOfSequence() {
+    var arr = [], arrN = [], i, len, minNumb, MaxNumb;
     var str = document.getElementById("input-text").value;
-    var arr = [], i=0;
-    while (str != undefined && str != "") {
-        arr[i] = 
+    arr = str.split(' ').valueOf();
+    len = arr.length, minNumb = +arr[0], maxNumb = +arr[0];
+    for (i = 0; i < len; i+=1) {
+        if (+arr[i] < minNumb) {
+            minNumb = arr[i];
+        }
+        if (+arr[i] > maxNumb) {
+            maxNumb = arr[i];
+        }
     }
+    arr.sort(function (a, b) {
+        return a - b;
+    });
 
-    
+    jsConsole.writeLine("Max number is: " + maxNumb + ";  Min number is: " + minNumb);
+    jsConsole.writeLine("Max number is: " + arr[len-1] + ";  Min number is: " + arr[0]);
 }
---
+++ Home4Loops/Loops/scripts/4.LexicographicallySmallest.js
@@ -0,0 +1,23 @@
+﻿/*•	Write a script that finds the lexicographically smallest and largest property in document, window and navigatorobjects.*/
+
+function lexicographicallySmallestAndLargest(obj) {
+    var obj = obj || {};
+    obj = document.getElementById("input-text").value;
+    var prop, minProp = obj[0], maxProp = obj[0];
+    for (prop in obj) {
+        if (prop < minProp) {
+            minProp = prop;
+        }
+        if (prop > maxProp) {
+            maxProp = prop;
+        }
+    }
+
+    obj = window.document;
+    lexicographicallySmallestAndLargest(obj);
+    jsConsole.writeLine("Min property in " + obj + " is: " + minProp + ";  Max property in " + obj + " is: " + maxProp);
+
+    
+    lexicographicallySmallestAndLargest(obj);
+    jsConsole.writeLine("Min property in " + obj + " is: " + minProp + ";  Max property in " + obj + " is: " + maxProp);
+}
