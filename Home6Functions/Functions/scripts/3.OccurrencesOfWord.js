1ab000269934d437b34e05b21b54c69b42c503d7
aliv59git <vilavaln@gmail.com>
2015-06-04 17:17:49 +0300

    new verd

--- Home6Functions/Functions/scripts/3.OccurrencesOfWord.js
+++ Home6Functions/Functions/scripts/3.OccurrencesOfWord.js
@@ -3,10 +3,19 @@
 •	Use function overloading.*/
 
 function occurrencesOfWord(text, isCaseSensitive) {
-    var text, words, count, len, i, word, isCaseSensitive; 
-    
-    text = jsConsole.read('#input-text');
+    var text, words, count, len, i, word, isCaseSensitive;
+    var isCaseSensitive = isCaseSensitive || false;
+
+    var text = 'Velit mollis odio SOLLicitudin lacinia aliquam posuere, sapien elementum lobortis tincidunt, turpis dui ornare nisl, sollicitudin interdum turpis nunc eget sem nulla eu ultricies orci praesent id augue nec lorem pretium congue sit amet ac nunc fusce iaculis lorem eu diam hendrerit at mattis purus dignissim vivamus mauris tellus, fringilla vel dapibus a, blandit quis erat vivamus elementum aliquam luctus etiam fringilla pretium sem vitae sodales mauris id nulla est praesent laoreet, metus vel auctor aliquam, eros purus vulputate leo, eget consequat neque quam id tellus duis ultricies tempor tortor, vitae dignissim ligula mattis nec in hac habitasse platea dictumst.';
+    text += jsConsole.read('#input-text');
     isCaseSensitive = jsConsole.read('#input-text1').value;
+
+    switch (arguments.length) {
+        case 1:
+            return countWords(words); break;
+        case 2:
+            return isCaseSensitive ? countWordsCS(words, isCaseSensitive) : countWords(words); break;
+    }
 
     function getWords(text) {
         words = text.split(/\W+/);
@@ -22,7 +31,7 @@ function occurrencesOfWord(text, isCaseSensitive) {
     var wordCount = countWords(words);
 
     function countWords(words) {
-        wordCount = {};
+        var wordCount = {};
         word = {};
         for (i in words) {
             word = words[i].toLowerCase();
@@ -34,40 +43,22 @@ function occurrencesOfWord(text, isCaseSensitive) {
         }
         return wordCount;
     }
-    //var text = 'Velit mollis odio sollicitudin lacinia aliquam posuere, sapien elementum lobortis tincidunt, turpis dui ornare nisl, sollicitudin interdum turpis nunc eget sem nulla eu ultricies orci praesent id augue nec lorem pretium congue sit amet ac nunc fusce iaculis lorem eu diam hendrerit at mattis purus dignissim vivamus mauris tellus, fringilla vel dapibus a, blandit quis erat vivamus elementum aliquam luctus etiam fringilla pretium sem vitae sodales mauris id nulla est praesent laoreet, metus vel auctor aliquam, eros purus vulputate leo, eget consequat neque quam id tellus duis ultricies tempor tortor, vitae dignissim ligula mattis nec in hac habitasse platea dictumst.';
 
 
-    //function countWords(words, isCaseSensitive) {
-    //    wordCount = {};
-    //    word = {};
-    //    for (var i in words) {
-    //        word = words[i];
-    //        if (wordCount[word]) {
-    //            wordCount[word] += 1;
-    //        } else {
-    //            wordCount[word] = 1;
-    //        }
-    //    }
-    //    return wordCount;
-    //}
+    function countWordsCS(words, isCaseSensitive) {
+        var wordCount = {};
+        word = {};
+        for (var i in words) {
+            word = words[i];
+            if (wordCount[word]) {
+                wordCount[word] += 1;
+            } else {
+                wordCount[word] = 1;
+            }
+        }
+        return wordCount;
+    }
 
-    //function occurrencesOfWordCaseSensitive(word) {
-    //    count = 0;
-
-    //    for (i = 0; i < len; i += 1) {
-    //        if (words[i] === word) {
-    //            count += 1;
-    //        }
-    //    }
-    //    return count;
-    //}
-
-    //switch (arguments.length) {
-    //    case 1:
-    //        return countWords(words); break;
-    //    case 2:
-    //        return isCaseSensitive ? countWords(words, isCaseSensitive) : countWords(words); break;
-    //}
     
     for (var word in wordCount) {
         jsConsole.writeLine(word + ' -> ' + wordCount[word]);
