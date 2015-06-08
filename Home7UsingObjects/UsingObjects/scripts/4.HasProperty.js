e012bf4b0fad1e3f90f42336f420c5789afce87a
aliv59git <vilavaln@gmail.com>
2015-06-08 16:36:57 +0300

    new homev

--- Home7UsingObjects/UsingObjects/scripts/4.HasProperty.js
+++ Home7UsingObjects/UsingObjects/scripts/4.HasProperty.js
@@ -3,16 +3,44 @@
 •	var hasProp = hasProperty(obj, 'length');*/
 
 function hasProperty() {
-    var obj;
+    var obj = {fName: 'Pesho', lName: 'Peshev', 
+        marks: [{subject: 'Biology', score: 5.50},
+            {subject: 'English', score: 4.90},
+            {subject: 'Physics', score: 3.50}],
+        friends: ['Pepi Achev', 'Iva Kirova', 'Tczvetan Ivanov', 'Suzi', 'Emili'],
+        papers: {name: 'Envirenment of animals in...', authors: 'Pepi Achev, Pesho Peshev'}},
+        prop, str, length, result = [];
+
+    jsConsole.writeLine(hasPropertyInObj(obj, 'length'));
+    jsConsole.writeLine(hasPropertyInObj(obj, 'lName'));
+    jsConsole.writeLine(hasPropertyInObj(obj, 'marks'));
+    jsConsole.writeLine(hasPropertyInObj(obj, 'score'));
+    jsConsole.writeLine(hasPropertyInObj(obj, 'subject'));
+    jsConsole.writeLine(hasPropertyInObj(obj, 'authors'));
 
 
-    var hasProp(obj, 'length'){
+    function hasPropertyInObj(object, property) {
 
+        for (var prop in object) {
+            if (typeof (object[prop]) === 'object') {
+                for (var i in object[prop]) {
+                    if (i === property) {
+                        return true;
+                    }
+                };
+            } else if (Array.isArray(object[prop])) {
+                for (var i = 0, len = object[prop].length; i < len; i+=1) {
+                    if (object[prop][i] === property) {
+                        return true;
+                    }
+                }
+            }
+            if (prop === property) {
+                return true;
+            }
+        }
+        return false;
     }
-
-
-
-
 }
 
 
---
+++ Home7UsingObjects/UsingObjects/scripts/5.YoungestPerson.js
@@ -0,0 +1,38 @@
+﻿/*•	Write a function that finds the youngest person in a given array of people 
+and prints his/hers full name
+•	Each person has properties firstname, lastname and age.*/
+
+function youngestPerson() {
+    var people, len, index = 0, minAge = Number.MAX_VALUE;
+    people = [
+    { fName: 'Iva', lName: 'Kirova', age: 18 },
+    { fName: 'Ivan', lName: 'Peshev', age: 21 },
+    { fName: 'Krasi', lName: 'Kirov', age: 41 },
+    { fName: 'Alis', lName: 'Tager', age: 27 },
+    { fName: 'Ivanka', lName: 'Tasheva', age: 17 },
+    { fName: 'Geri', lName: 'Ilieva', age: 23 },
+    { fName: 'Sasho', lName: 'Dobrev', age: 28 },
+    ];
+
+
+    for (var item in people) {
+        if (people[item].age < minAge) {
+            minAge = people[item].age;
+            index = item;
+        }
+    }
+    printName(index);
+
+
+    function printName(index){
+        jsConsole.writeLine('The youngest person is '+people[index].fName+' '+people[index].lName+'.');
+    }
+}
+
+
+
+
+
+
+
+
