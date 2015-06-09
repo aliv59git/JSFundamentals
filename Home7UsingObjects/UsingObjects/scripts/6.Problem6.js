8714c4818451258acde625f19c263809357ed675
aliv59git <vilavaln@gmail.com>
2015-06-09 12:07:10 +0300

    new h

--- Home7UsingObjects/UsingObjects/scripts/6.Problem6.js
+++ Home7UsingObjects/UsingObjects/scripts/6.Problem6.js
@@ -5,35 +5,62 @@
 function grouping() {
     var people, i, arr, sorted;
 
-    people = [
-    { fName: 'Iva', lName: 'Kirova', age: 18 },
-    { fName: 'Ivan', lName: 'Peshev', age: 21 },
-    { fName: 'Krasi', lName: 'Kirov', age: 41 },
-    { fName: 'Alis', lName: 'Tager', age: 27 },
-    { fName: 'Ivanka', lName: 'Tasheva', age: 17 },
-    { fName: 'Geri', lName: 'Ilieva', age: 23 },
-    { fName: 'Sasho', lName: 'Dobrev', age: 28 },
-    ];
-
-    function buildPerson(fName, lName, age){
+    function createPerson(fName, lName, age) {
         return {
             fName: fName,
             lName: lName,
             age: age,
-            toString: function toString(){
-                return fName + ' ' + lName + 'age: ' + age;
+            toString: function toString() {
+                jsConsole.writeLine(fName + ' ' + lName + ' age: ' + age);
             }
         };
     }
-    var firstNames = [];
-    for (var i = 0, len = people.length; i < len; i+=1) {
-        firstNames.push(people[i].fName);
+
+    var people = [createPerson('Iva', 'Kirova', 18),
+        createPerson('Ivan','Peshev', 21),
+        createPerson('Krasi', 'Kirov', 41),
+        createPerson('Alis', 'Tager', 27),
+        createPerson('Ivanka', 'Tasheva', 17),
+        createPerson('Geri', 'Ilieva', 23),
+        createPerson('Sasho', 'Dobrev', 28)];
+
+    console.log(people.length);
+    var len = people.length;
+    function printPeople(people) {
+        for (var i = 0; i < len; i += 1) {
+            people[i].toString();
+        }
     }
-    var sorted = firstNames.sort();
-    for (var j = 0, len = sorted.length; j < len; j+=1) {
-        var peopleFName = [];
+
+    function groupPeople(arr, key) {
+        arr.sort(function (p1, p2) {
+            return p1.key > p2.key;
+        })
+    }
+
+    printPeople(groupPeople(people, people.fName));
+
+
+
+    //function buildPerson(fName, lName, age){
+    //    return {
+    //        fName: fName,
+    //        lName: lName,
+    //        age: age,
+    //        toString: function toString(){
+    //            return fName + ' ' + lName + 'age: ' + age;
+    //        }
+    //    };
+    //}
+    //var firstNames = [];
+    //for (var i = 0, len = people.length; i < len; i+=1) {
+    //    firstNames.push(people[i].fName);
+    //}
+    //var sorted = firstNames.sort();
+    //for (var j = 0, len = sorted.length; j < len; j+=1) {
+    //    var peopleFName = [];
         
-    }
+    //}
 
 
     console.log(firstNames);
@@ -47,13 +74,13 @@ function grouping() {
     //console.log(group(people, lName));
     //console.log(group(people, age));
 
-    function group(arr, key) {
-        var sorted = arr[key].sort(function (a, b) {
-            return a - b;
-        });
-        return sorted;
-    }
-
+    //function group(arr, key) {
+    //    var sorted = arr[key].sort(function (a, b) {
+    //        return a - b;
+    //    });
+    //    return sorted;
+    //}
+    //console.log(person.forEach(group(person, fName)));
 }
 
 
