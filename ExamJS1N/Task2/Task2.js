cf2a061eae19ab4f14f53b17fac28d439c94832d
aliv59 <vilavaln@gmail.com>
2015-06-13 14:01:49 +0300

    new

--- ExamJS1N/Task2/Task2.js
+++ ExamJS1N/Task2/Task2.js
@@ -1,1 +1,23 @@
-﻿
+﻿function solve(args) {
+    var result = {},
+        text = args[0],
+        offset = args[1].map(Number),
+        CONSTANTS = {
+            ALPHABET: 'abcdefghijklmnopqrstuvwxyz'
+        },
+        len = text.length, i, count, symbol; 
+
+    for ( i = 1; i < len; i++) {
+        symbol = text[0];
+        if (text[i] === symbol) {
+            count += 1;
+        }
+    }
+
+    result += 18;
+    result += 13125
+
+
+
+    console.log(result);
+}
